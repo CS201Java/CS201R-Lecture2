48afac3fa64d847eeefc9a7dae3a6bffd70e2d97
jjgr29 <jjgr29@umsystem.edu>
2025-08-06 15:05:07 -0500

    Update

--- Main.java
+++ Main.java
@@ -36,6 +36,8 @@ public class Main {
         int i = (int)'A'; 	// The Unicode of character A is assigned to i
         System.out.println(i); 
 
+        //Math Functions
+        //Random
         for (int j = 0; j < 20; j++)
             System.out.print((int)(Math.random() * 10) + " ");		//Returns a random integer between 0 and 9.
 
@@ -45,9 +47,16 @@ public class Main {
             System.out.print(50 + (int)(Math.random() * 50) + " "); 		//Returns a random integer between 50 and 99.
         System.out.println();
 
+        //Max
+
+        System.out.println("\n\nThe maximum of 30 and 54 is: " + Math.max(30,54));
+        System.out.println("The maximum of 3.0 and 5.4 is: " + Math.max(3.0,5.4));
+        System.out.println("The maximum of 30 and 5.4 is: " + Math.max(30,5.4));
+        System.out.println("The maximum of 30, 92 and 54 is: " + Math.max(Math.max(30,92),54));
+
         //Date & Date Formatting
         LocalDate currentDate = LocalDate.now();
-        System.out.println("Today's date: " + currentDate);	
+        System.out.println("\nToday's date: " + currentDate);	
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
         String formattedDate = currentDate.format(formatter);
         System.out.println("Formated date: " + formattedDate);
